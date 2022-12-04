d3c8f3ef9f5298d70ad1eb0f1a36040f7d5a3014
93nezyr <julen5raqukm@gmail.com>
2022-12-04 18:06:50 +0900

    Add simlib/ais.rs

--- src_rust/src01/simlib/src/ais.rs
+++ src_rust/src01/simlib/src/ais.rs
@@ -1,2 +1,19 @@
+mod env;
+mod agent;
+use env::Env;
+use agent::Agent;
 
+pub struct Thinker {
+    env: Env,
+    agent: Agent,
+}
+
+impl Thinker {
+    pub fn new() -> Thinker {
+        Thinker {
+            env: Env::new(),
+            agent: Agent::new(),
+        }
+    }
+}
 
